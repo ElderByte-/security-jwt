b20bc018114f1df6f2edb5c51409d7e255a31e66
IsNull <p.buettiker@elderbyte.com>
2017-03-17 11:06:03 +0100

    fixed configuration

--- src/main/java/com/elderbyte/warden/spring/local/WardenSpringSecurityJwtAutoConfiguration.java
+++ src/main/java/com/elderbyte/warden/spring/local/WardenSpringSecurityJwtAutoConfiguration.java
@@ -12,6 +12,7 @@ import com.elderbyte.warden.spring.local.jwt.JwtTokenConverter;
 import com.elderbyte.warden.spring.rsa.RSAPublicKeyProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -21,7 +22,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
 @Configuration
+@EnableConfigurationProperties
 @Import({
+        WardenSpringSecurityJwtSettings.class,
         AccessDeniedExceptionHandler.class,
         DefaultWardenSecurityConfig.class,
 })

9ad2c11e63832de7cbfbb7c6624ff5e790f14b94
Pascal Büttiker <pascalbuettiker@bluewin.ch>
2017-12-05 21:06:27 +0100

    upgraded to java 9 + spring boot 2

--- src/main/java/com/elderbyte/warden/spring/local/config/DefaultWardenWebSecurityConfiguration.java
+++ src/main/java/com/elderbyte/warden/spring/local/config/DefaultWardenWebSecurityConfiguration.java
@@ -8,7 +8,10 @@ import com.elderbyte.warden.spring.WardenSpringSecurityJwtSettings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -29,6 +32,13 @@ public class DefaultWardenWebSecurityConfiguration extends WebSecurityConfigurer
 
     @Autowired(required = false)
     private MockJwtHolder mockJwtHolder;
+
+
+    @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {

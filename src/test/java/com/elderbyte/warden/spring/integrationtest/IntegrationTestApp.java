8db8680d139af9d9f9b958c6686972d7d6cb4be9
Pascal Büttiker <pascalbuettiker@bluewin.ch>
2018-01-23 09:58:08 +0100

    support local test app with web

--- src/test/java/com/elderbyte/warden/spring/integrationtest/IntegrationTestApp.java
+++ src/test/java/com/elderbyte/warden/spring/integrationtest/IntegrationTestApp.java
@@ -5,10 +5,18 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.net.UnknownHostException;
 
-
+@EnableWebSecurity
+@RestController
+@Secured("USER")
+@RequestMapping("/api")
 @SpringBootApplication
 public class IntegrationTestApp {
 
@@ -21,5 +29,11 @@ public class IntegrationTestApp {
 
         SpringApplication app = new SpringApplication(IntegrationTestApp.class);
         Environment env = app.run(args).getEnvironment();
+    }
+
+
+    @GetMapping
+    public String test(){
+        return "foo";
     }
 }

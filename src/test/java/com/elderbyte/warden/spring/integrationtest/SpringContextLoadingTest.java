c12b474f088fc926b935d460532b848129dff1c5
Pascal Büttiker <pascalbuettiker@bluewin.ch>
2018-01-23 16:09:18 +0100

    incremented version & release

--- src/test/java/com/elderbyte/warden/spring/integrationtest/SpringContextLoadingTest.java
+++ src/test/java/com/elderbyte/warden/spring/integrationtest/SpringContextLoadingTest.java
@@ -8,11 +8,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = IntegrationTestApp.class)
+@TestPropertySource(properties = {
+        "warden.client.enableMock=false"
+})
 public class SpringContextLoadingTest {
 
     @Autowired

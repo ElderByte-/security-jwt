9704f41839e9d9bb374c90fa44d4bac742f52f54
Pascal Büttiker <pascalbuettiker@bluewin.ch>
2018-02-15 14:15:47 +0100

    improved unit tests to check presence of filters

--- src/test/java/com/elderbyte/warden/spring/integrationtest/SpringContextLoadingTest.java
+++ src/test/java/com/elderbyte/warden/spring/integrationtest/SpringContextLoadingTest.java
@@ -3,11 +3,15 @@ package com.elderbyte.warden.spring.integrationtest;
 import com.elderbyte.warden.spring.WardenSpringSecurityJwtSettings;
 import com.elderbyte.warden.spring.local.auth.LocalAuthService;
 import com.elderbyte.warden.spring.local.auth.SecurityUtils;
+import com.elderbyte.warden.spring.local.jwt.JwtAuthenticationFilter;
+import com.elderbyte.warden.spring.mock.MockAuthenticationFilter;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -25,6 +29,9 @@ public class SpringContextLoadingTest {
     @Autowired
     private WardenSpringSecurityJwtSettings settings;
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @Test
     public void contextLoads(){
         Assert.assertTrue(authService != null);
@@ -39,5 +46,16 @@ public class SpringContextLoadingTest {
     @Test
     public void ensureMockNotLoaded(){
         Assert.assertTrue("There should be no authentication context when mock is disabled!",SecurityUtils.getAuthentication() == null);
+    }
+
+    @Test(expected = NoSuchBeanDefinitionException.class)
+    public void ensureMockAuthenticationFilterIsNotPresent(){
+        applicationContext.getBean(MockAuthenticationFilter.class); // Expect bean to be missing
+
+    }
+
+    @Test
+    public void ensureStandardJwtFilterIsPresent(){
+        Assert.assertTrue("JwtAuthenticationFilter must be present when mock is enabled!", applicationContext.getBean(JwtAuthenticationFilter.class) != null);
     }
 }
--- src/test/java/com/elderbyte/warden/spring/integrationtest/TestMockConfiguration.java
+++ src/test/java/com/elderbyte/warden/spring/integrationtest/TestMockConfiguration.java
@@ -2,12 +2,16 @@ package com.elderbyte.warden.spring.integrationtest;
 
 import com.elderbyte.warden.spring.WardenSpringSecurityJwtSettings;
 import com.elderbyte.warden.spring.local.auth.SecurityUtils;
+import com.elderbyte.warden.spring.local.jwt.JwtAuthenticationFilter;
+import com.elderbyte.warden.spring.mock.MockAuthenticationFilter;
 import com.elderbyte.warden.spring.mock.MockJwtHolder;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -17,13 +21,16 @@ import org.springframework.test.context.junit4.SpringRunner;
 @TestPropertySource(properties = {
         "warden.client.enableMock=true"
 })
-public class TestMockUser {
+public class TestMockConfiguration {
 
     @Autowired
     private WardenSpringSecurityJwtSettings settings;
 
     @Autowired
     private MockJwtHolder mockJwtHolder;
+
+    @Autowired
+    private ApplicationContext applicationContext;
 
     @Test
     public void ensureMockEnabled(){
@@ -33,5 +40,15 @@ public class TestMockUser {
     @Test
     public void ensureMockLoaded(){
         Assert.assertTrue("There should be a mocked authentication present!", SecurityUtils.getAuthentication() != null);
+    }
+
+    @Test
+    public void ensureMockAuthenticationFilterIsPresent(){
+        Assert.assertTrue("MockAuthenticationFilter must be present when mock is enabled!", applicationContext.getBean(MockAuthenticationFilter.class) != null);
+    }
+
+    @Test(expected = NoSuchBeanDefinitionException.class)
+    public void ensureStandardJwtFilterIsNotPresent(){
+        applicationContext.getBean(JwtAuthenticationFilter.class); // Expect bean to be missing
     }
 }
